a309d4e7c313874ac378b242252e08560c0ebf6b
AlirezaaaRd <alireza.rd.78@gmail.com>
2024-10-14 14:48:04 +0330

    v0.44

--- Didikala/settings.py
+++ Didikala/settings.py
@@ -66,7 +66,7 @@ import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'mainapp', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
